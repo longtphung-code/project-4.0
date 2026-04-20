3a4d4af9460e6221387108f18e42fe6276fa3151
longtphung-code <longtphung@gmail.com>
2026-04-20 19:47:50 -0400

    Project 4 part 2 completed

--- project-4/project-4.cpp
+++ project-4/project-4.cpp
@@ -1,48 +1,47 @@
 #include <iostream>
+#include <cmath> // for ceiling
+#include <iomanip> //for setprecision
 using namespace std;
 
+
 int main() {
-	int month, year;
-	int days = 0;
+	double weight, distance;
+	double rate = 0.0;
+	double totalCost;
 
-	//Ask the user for the year
-	cout << "Enter a month (1-12): ";
-	cin >> month;
+	//Get user input for weight and distance
+	cout << "Enter package weight (kg) : ";
+	cin >> weight;
 
-	//Input for month
-	if (month < 1 || month >12) {
-		cout << "Error: Invalid month. Please enter a value between 1 and 12." << endl;
+	cout << "Enter shipping distance (miles): ";
+	cin >> distance;
+
+	// Input validation
+	if (weight <= 0 || distance > 3000) {
+		cout << "Error: Distance must be between 10 and 3000 miles." << endl;
 		return 0;
 	}
 
-	cout << "Enter a year: ";
-	cin >> year;
-		
-	// Determine if it's a leap year
-	bool isLeapYear = false;
-
-	if (year % 100 ==0) {
-		if (year % 400 == 0) {
-			isLeapYear = true;
-		}
-	} else if (year % 4 == 0) {
-		isLeapYear = true;
+	// Determine the rate based on weight
+	if (weight <=2) {
+		rate = 1.10;
+	}else if (weight <= 6) {
+		rate = 2.20;
+	} else if (weight <= 10) {
+		rate = 3.70;
+	} else { // up to 20 kg)
+		rate = 4.80;
 	}
 
-	// Determine the number of days in the month
-	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-		days = 31;
-	} else if (month == 4 || month == 6 || month == 9 || month == 11) {
-		days = 30;
-	} else if (month == 2) {
-		if (isLeapYear) {
-			days = 29;
-		} else {
-			days = 28;
-		}
-	}
-	// Output the result 
-	cout << "Number of days: " << days << endl;
+	// Calculate number of 500 mile segments
+	int segments = ceil(distance / 500.0);
+
+	//Calculate total cost
+	totalCost = segments * rate;
+
+	// Output results
+	cout << fixed << setprecision(2); // Set precision for currency format
+	cout << "Shiping cost: $" << totalCost << endl;
 
 	return 0;
 }
